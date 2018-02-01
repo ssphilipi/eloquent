3e020c9304b424277fdd58c1e31b670841373c95
Philipi <philipisiqueira@ejcm.com.br>
2018-02-01 11:45:07 -0200

    + eloquent

--- app/Http/Controllers/Aluno.php
+++ app/Http/Controllers/Aluno.php
@@ -13,24 +13,51 @@ class AlunoController extends Controller
     public function index()
     {
         $alunos = Aluno::all();
-      
+
         return view('alunos',['alunos' => $alunos]);
     }
 
 
     public function insereAluno(Request $request)
     {
+        $aluno_novo = new Aluno;
+
+        $aluno_novo->nome = $request->nome;
+        $aluno_novo->registro = $request->registro;
+        $aluno_novo->serie = $request->serie;
+        $aluno_novo->turma = $request->turma;
+        $aluno_novo->faltas = $request->faltas;
+        // $aluno_novo->status = $request->status;
+        $aluno_novo->media = $request->media;
+
+        $aluno_novo->save();
+
         return back();
     }
 
 
     public function atualizaAluno(Request $request, $id)
     {
+        $aluno = Aluno::find($id);
+
+        $aluno->nome = $request->nome;
+        $aluno->serie = $request->serie;
+        $aluno->turma = $request->turma;
+        $aluno->media = $request->media;
+        $aluno->faltas = $request->faltas;
+
+        $aluno->save();
+
         return back();
     }
 
-    public function deletaAluno($id) 
+    public function deletaAluno($id)
     {
+
+        // $aluno = Aluno::find($id);
+        // $aluno->delete();
+
+        Aluno::destroy($id);
 
         return back();
     }
@@ -40,7 +67,7 @@ class AlunoController extends Controller
     public function getAluno(Request $request){
 
         $aluno = new Aluno;
-        
+
         $resposta = $aluno->exemplo($request->id);
 
         dd($resposta);
